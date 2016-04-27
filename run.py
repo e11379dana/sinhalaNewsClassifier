01acd4d4d3554cfb28f537ce357bb51a0a239e25
namodaya <namodayabalaarachchi@gmail.com>
2016-04-27 14:52:59 +0530

    Create restfull web service

---
+++ run.py
@@ -0,0 +1,12 @@
+import flask
+from flask_restful import Api
+
+from NewsList.index import *
+
+app = flask.Flask(__name__)
+api = Api(app)
+
+api.add_resource(main, '/', endpoint='/')
+
+if __name__ == '__main__':
+    app.run(host="localhost", port=5000, debug=True)
