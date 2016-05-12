5961b433700f44cd5f44919a42daf8d5cf44a514
dilanka <dilanka.rathnayake@shipxpress.com>
2016-05-12 12:59:49 +0530

    front end template added

--- SNCR_BackEnd/NewsList/index.py
+++ SNCR_BackEnd/NewsList/index.py
@@ -1,4 +1,7 @@
+import urllib2
+
 import feedparser
+import requests
 from flask import *
 from flask_restful import Resource
 
@@ -7,7 +10,7 @@ from mysql.connector import (connection)
 
 class main(Resource):
     def get(self):
-        db = connection.MySQLConnection(user='root', password='1234',
+        db = connection.MySQLConnection(user='root', password='ilovepera',
                                          host='127.0.0.1',
                                          database='NewsData',
                                          charset='utf8')
@@ -23,8 +26,6 @@ class main(Resource):
                  title  VARCHAR(1000), link  VARCHAR(1000), description VARCHAR(1000), pubDate VARCHAR(1000)) ENGINE = InnoDB DEFAULT CHARSET=utf8"""
 
         cursor.execute(sql)
-
-
         feed = feedparser.parse('http://www.hirunews.lk/rss/sinhala.xml')
         for entry in feed['items']:
 
@@ -52,6 +53,5 @@ class main(Resource):
             })
 
         db.close()
-
         return (newsList)
 
