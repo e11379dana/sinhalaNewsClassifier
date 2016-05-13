2ee0086ef1dcc529f566ee0525ab173b1b3355b1
e11379dana <asdananjaya93@gmail.com>
2016-05-13 12:25:55 +0530

    Saving image link in database

--- SNCR_BackEnd/Aggregator/newsUpdater.py
+++ SNCR_BackEnd/Aggregator/newsUpdater.py
@@ -1,11 +1,16 @@
 import feedparser
+import urllib.request as urllib2
+
+import re
 import schedule
 import time
+
+from bs4 import BeautifulSoup
 from mysql.connector import (connection)
 
-db = connection.MySQLConnection(user='root', password='1234',
+db = connection.MySQLConnection(user='root', password='',
                                 host='127.0.0.1',
-                                database='NewsData',
+                                database='newsdata',
                                 charset='utf8')
 
 # prepare a cursor object using cursor() method
@@ -18,27 +23,37 @@ def job():
 
     # Create table as per requirement
     sql = """CREATE TABLE NewsOrder (
-                title  VARCHAR(1000), link  VARCHAR(1000), description VARCHAR(1000), pubDate VARCHAR(1000), category VARCHAR(10)) ENGINE = InnoDB DEFAULT CHARSET=utf8"""
+                title  VARCHAR(1000), link  VARCHAR(1000), description VARCHAR(1000), pubDate VARCHAR(1000), imgLink VARCHAR(11000)) ENGINE = InnoDB DEFAULT CHARSET=utf8"""
 
     cursor.execute(sql)
 
 
     feed = feedparser.parse('http://www.hirunews.lk/rss/sinhala.xml')
     for entry in feed['items']:
-        description = entry['description'].split("..")
-        print description
-        sql = "INSERT INTO NewsOrder(title,link,description) VALUES ('%s','%s','%s') " % (entry['title'],entry['link'],description[0])
-        #print (entry['link'])
+
+        # converting title into a url
+        title = entry['title'].replace(" ", "")
+        asci = ascii(title)
+        urlTitle = asci.replace("\\", "%")
+        url = entry['link'] + '/' + urlTitle
+        content = urllib2.Request(url)
+        res = urllib2.urlopen(content).read()
+        soup = BeautifulSoup(res, "html.parser")
+        rows = soup.find_all('div', attrs={"class": "latest-pic"})
+        ans = re.findall('"([^"]*)"', str(rows))
+        print(ans[2])
+
+        sql = "INSERT INTO NewsOrder(title,link,description,imgLink) VALUES ('%s','%s','%s','%s') " % (
+        entry['title'], entry['link'], entry['description'],ans[2])
+
         try:
             cursor.execute(sql)
             db.commit()
-            print "Schedular is running"
         except:
             # Rollback in case there is any error
             db.rollback()
 
-
-schedule.every(10).minutes.do(job)
+schedule.every(0.1).minutes.do(job)
 
 while 1:
     schedule.run_pending()
